34b5605459fca035a8670cb21546b4c39281f013
Jean Jung <jean.jung@rocketmail.com>
2016-10-20 14:44:04 -0200

    Add test classes

---
+++ SpaceNamedClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Foo\Bar;
+
+/**
+ * Created by PhpStorm.
+ * User: jean
+ * Date: 20/10/16
+ * Time: 14:39
+ */
+class SpaceNamedClass
+{
+	public static function tryIt() {
+
+	}
+}
